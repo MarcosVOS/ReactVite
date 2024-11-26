8787c476200575b178eea5043fedb70605494cd2
MarcosVOS <marcos.oliveiraDEV@gmail.com>
2024-11-26 10:56:14 -0300

    joke

--- src/utils/Auth.ts
+++ src/utils/Auth.ts
@@ -1,5 +1,5 @@
 const users = [
-    { name: "A mestre do frontend Julia", email: 'julia@julia.com', password: 'julia1234' },
+    { name: "A mestre do frontend Julia - Cpa Autralia?", email: 'julia@julia.com', password: 'julia1234' },
     { name: "lucas", email: 'lucas@lucas.com', password: 'lucas1234' },
     { name: "o rolezeiro renan", email: 'renan@renan.com', password: 'renan' },
     { name: "stefany", email: 'stefany@stefany.com', password: 'stefany1234' },
